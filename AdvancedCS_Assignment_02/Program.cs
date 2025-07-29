6ae028a51929e2d2961c700c79a758e07d3df6f8
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-30 01:03:36 +0300

    Solve Q1

--- AdvancedCS_Assignment_02/Program.cs
+++ AdvancedCS_Assignment_02/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-
+            #region Q1
+            Console.Write("Enter Size : ");
+            int size = int.Parse(Console.ReadLine()!);
+            Console.Write("Enter Number Query : ");
+            int QueryNum = int.Parse(Console.ReadLine()!);
+            List<int> list = new(size);
+            Console.WriteLine("Enter Numbers : ");
+            for (int i = 0; i < list.Capacity; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()!));
+            }
+            List<int> query = new(QueryNum);
+            Console.WriteLine("Enter Queries : ");
+            for (int i = 0; i < query.Capacity; i++)
+            {
+                query.Add(int.Parse(Console.ReadLine()!));
+            }
+            int count = 0;
+            Console.WriteLine("Output : ");
+            for (int i = 0; i < query.Count; i++)
+            {
+                count = 0;
+                for (int j = 0; j < list.Count; j++)
+                    if (list[j] > query[i])
+                        count++;
+                Console.WriteLine(count);
+            }
+            #endregion
             Console.ReadKey();
         }
     }
